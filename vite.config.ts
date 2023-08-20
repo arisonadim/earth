a6b281851ebd2575d5e2e70475e8a52295fbb70c
Dmitry Ermilov <ermilov.dmitry@gmail.com>
2023-08-21 00:24:55 +0700

    [add] configs & package.json

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, normalizePath } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      'Styles': path.resolve(__dirname, 'src/assets/styles'),
+      'Images': path.resolve(__dirname, 'src/assets/images'),
+      'Fonts': path.resolve(__dirname, 'src/assets/fonts'),
+      'Components': path.resolve(__dirname, 'src/components')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '${normalizePath(
+          path.resolve(__dirname, 'src/assets/styles/import.scss')
+        )}';`
+      }
+    }
+  }
+})
+
+
