77fe84bd968c5dc9d0bacc9d212f4ea70af2efa3
Dmitry Ermilov <ermilov.dmitry@gmail.com>
2023-08-21 02:19:12 +0700

    [add] deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,23 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/earth/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      'Styles': path.resolve(__dirname, 'src/assets/styles'),
-      'Images': path.resolve(__dirname, 'src/assets/images'),
-      'Fonts': path.resolve(__dirname, 'src/assets/fonts'),
-      'Components': path.resolve(__dirname, 'src/components')
-    }
+      Styles: path.resolve(__dirname, 'src/assets/styles'),
+      Images: path.resolve(__dirname, 'src/assets/images'),
+      Fonts: path.resolve(__dirname, 'src/assets/fonts'),
+      Components: path.resolve(__dirname, 'src/components'),
+    },
   },
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `@import '${normalizePath(
           path.resolve(__dirname, 'src/assets/styles/import.scss')
-        )}';`
-      }
-    }
-  }
+        )}';`,
+      },
+    },
+  },
 })
-
-
